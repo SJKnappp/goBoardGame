9c476c1852cd2f716ca10aedccc930c1a144d496
Samuel Knapp <samuel.j.knapp@btinternet.com>
2019-11-11 19:10:15 +0000

    detects when piece is on the board white still not able to play

--- main.cpp
+++ main.cpp
@@ -63,15 +63,14 @@ int main(){
     std::cin >> input;
     acceptedVal = false;
     while(acceptedVal == false){
-      if(input.length() == 2 || input.length() == 3){acceptedVal = true;
+      if(input.length() == 2 || input.length() == 3){
         x_at = input.at(0) - 97;
         y_at = input.at(1) - 49;
-        if(input.length() == 3){y_ten = input.at(3) - 49;
-          y_at = 10*y_at + y_ten;}
-        if((x_at < 0 || y_at < 0 || x_at > 19 || y_at > 19) && board.at(y_at*boardSize+x_at).state == 0){ break; }
+        if(input.length() == 3){y_ten = input.at(3) - 49;  y_at = 10*y_at + y_ten;}
+        std::cout << x_at << y_at << '\n';
+        if(x_at >= 0 && y_at >= 0 && x_at < 19 && y_at < 19 && board.at(y_at*boardSize+x_at).state == 0){ break; }
       }
-      std::cout << "please renter value ";std::cin >> input;
-
+      std::cout << "please renter value "; std::cin >> input;
     }
 
     board.at(x_at + y_at*boardSize) = updatePiece(board, x_at, y_at, boardSize, isBlack);
