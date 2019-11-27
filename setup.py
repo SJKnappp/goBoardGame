3723a7ee5b740260cd42a90a0254306edec382ae
Samuel Knapp <samuel.j.knapp@btinternet.com>
2019-11-27 21:08:41 +0000

    removed python files

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-setup(ext_modules=[Extension("boardWrapper",
-                    ["boardWrapper.pyx", "boardWrapper.cpp"],
-                    language="c++",)], cmdclass = {'build_ext': build_ext})
